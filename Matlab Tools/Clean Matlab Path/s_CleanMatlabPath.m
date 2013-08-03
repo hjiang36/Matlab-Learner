ed91f5305ddbd1aa4a36a15b96d08965257c269b
Haomiao Jiang <hjiang36@gmail.com>
2013-08-03 01:44:58 -0700

    Add clean up tools
    Clean up .svn and .git folder from matlab search path

---
+++ Matlab Tools/Clean Matlab Path/s_CleanMatlabPath.m
@@ -0,0 +1,20 @@
+%% s_CleanMatlabPath
+%    clean up matlab path file, namely, clean up .svn and .git folders from
+%    the matlab search path. I believe we won't need these in Matlab search
+%    path.
+
+%% Load and Parse Matlab Path
+curPath = matlabpath;
+curPath = strsplit(curPath,':');
+
+%% Taversal through path name
+matchStr = {'.svn','.git'};
+for i = 1 : length(curPath)
+    for j = 1 : length(matchStr)
+        if ~isempty(strfind(curPath{i},matchStr{j}))
+            disp(['Removing Path:' curPath{i}]);
+            rmpath(curPath{i});
+            break;
+        end
+    end
+end
